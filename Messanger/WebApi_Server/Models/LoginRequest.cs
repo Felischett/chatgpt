15e81d07bebe75b570353ad282607835571bbcee
Felischett <165804735+Felischett@users.noreply.github.com>
2026-03-18 14:29:56 +0100

    del

--- Messanger/WebApi_Server/Models/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApi_Server.Models
-{
-    public class LoginRequest
-    {
-        public string Email { get; set; } = "";
-        public string Password { get; set; } = "";
-    }
-}
